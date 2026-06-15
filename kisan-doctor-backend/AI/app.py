44c681f1906d45875920ec5d98cd013325bad108
Shaheer Ahmed <your-email@gmail.com>
2026-06-16 03:36:27 +0500

    Deploy AI service to Render

--- kisan-doctor-backend/AI/app.py
+++ kisan-doctor-backend/AI/app.py
@@ -187,11 +187,13 @@ def predict():
 # -----------------------------------
 # Run App
 # -----------------------------------
-
 if __name__ == "__main__":
+
+    port = int(
+        os.environ.get("PORT", 5001)
+    )
 
     app.run(
         host="0.0.0.0",
-        port=5001,
-        debug=True
+        port=port
     )
