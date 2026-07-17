c41d0fac89323562cf1fee9e8aa23d7a1f8ad2c7
Shaheer1786 <shaheerwa345@gmail.com>
2026-07-17 17:06:08 +0500

    index updated

--- kisan-doctor-backend/index.js
+++ kisan-doctor-backend/index.js
@@ -732,5 +732,9 @@ app.get("/admin/users", async (req, res) => {
   }
 
 });
-//Predict
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
